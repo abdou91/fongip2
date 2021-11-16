9b6cc9c857b01656c7b17db84c0dad95be2ff749
Abdou Mbar Ly <abdoumbarly@gmail.com>
2021-11-16 16:46:21 +0000

    convert excel date function

--- financing/models/excel_utility.py
+++ financing/models/excel_utility.py
@@ -3,10 +3,16 @@
 from datetime import datetime
 import xlrd
 
-def convert_excel_date_to_python_date(xl_date):
+"""def convert_excel_date_to_python_date(xl_date):
 	if isinstance(xl_date,int):
 		datetime_date = xlrd.xldate_as_datetime(xl_date, 0)
 		date_object = datetime_date.date()
 		string_date = date_object.isoformat()
 		return string_date
-	return 
+	return""" 
+
+# convert excel date to python date
+def convert_excel_date_to_python_date(excel_date):
+  dt = datetime.fromordinal(datetime(1900, 1, 1).toordinal() + int(excel_date) - 2)
+  dt.strftime('%Y-%m-%d')
+  return str(dt)
